00c957c31f5ed1bd5f8f4664acc598042f7dbab5
Kahviz <joakimukkonen@gmail.com>
2026-03-12 17:40:27 +0200

    Add project files.

---
+++ UntilitedMathLibrary.cpp
@@ -0,0 +1,12 @@
+﻿// UntilitedMathLibrary.cpp : Defines the entry point for the application.
+//
+
+#include "UntilitedMathLibrary.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ UntilitedMathLibrary.h
@@ -0,0 +1,8 @@
+﻿// UntilitedMathLibrary.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
