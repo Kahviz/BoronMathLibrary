6d86d046b71851407e402851aec4d09e8a068f5d
Kahviz <joakimukkonen@gmail.com>
2026-05-12 17:17:03 +0300

    s

--- BoronMath/BoronMath.h
+++ BoronMath/BoronMath.h
@@ -44,4 +44,8 @@
 #include "GPU/Functions/TransformGPUNormal.h"
 
 //Structs
-#include "Structs/Transform.h"
+struct Transform {
+    Vector3 Position{ 0,0,0 };
+    Vector3 Orientation{ 0,0,0 };
+    Vector3 Size{ 1,1,1 };
+};
--- BoronMath/Structs/Transform.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <BoronMath.h>
-
-struct Transform {
-    Vector3 Position{ 0,0,0 };
-    Vector3 Orientation{ 0,0,0 };
-    Vector3 Size{ 1,1,1 };
-};
